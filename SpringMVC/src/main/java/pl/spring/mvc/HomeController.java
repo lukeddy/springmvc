bae9fd22c11d86e4d9e8ae67e3760a050eae73d8
Pawel Kozbial <pawel.kozbial@gmail.com>
2013-02-04 16:45:10 +0100

    Dalsza zabawa z wyświetleniem w ui:repeat

--- SpringMVC/src/main/java/pl/spring/mvc/HomeController.java
+++ SpringMVC/src/main/java/pl/spring/mvc/HomeController.java
@@ -11,29 +11,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import pl.spring.mvc.service.CarManager;
+
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
+				DateFormat.LONG, locale);
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		CarManager carList = new CarManager();
+
+		model.addAttribute("serverTime", formattedDate);
+		model.addAttribute("carList", carList);
+
 		return "home";
 	}
-	
+
 }
---
+++ SpringMVC/src/main/java/pl/spring/mvc/service/CarManager.java
@@ -0,0 +1,44 @@
+package pl.spring.mvc.service;
+
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
+import pl.spring.mvc.model.Brand;
+import pl.spring.mvc.model.Car;
+
+public class CarManager {
+	private static List<Car> carList;
+
+	static {
+		Brand brand1 = new Brand();
+		brand1.setId((long) 1);
+		brand1.setName("Mercedes");
+		brand1.setCountry("Germany");
+
+		Brand brand2 = new Brand();
+		brand2.setId((long) 2);
+		brand2.setName("Peugeot");
+		brand2.setCountry("France");
+
+		Car car1 = new Car();
+		car1.setId((long) 1);
+		car1.setBrand(brand1);
+		car1.setModel("SL 500");
+		car1.setPrice(new BigDecimal(40000));
+
+		Car car2 = new Car();
+		car2.setId((long) 2);
+		car2.setBrand(brand2);
+		car2.setModel("607");
+		car2.setPrice(new BigDecimal(35000));
+
+		carList = new LinkedList<Car>();
+		carList.add(car1);
+		carList.add(car2);
+	}
+
+	public List<Car> getCarList() {
+		return carList;
+	}
+}
